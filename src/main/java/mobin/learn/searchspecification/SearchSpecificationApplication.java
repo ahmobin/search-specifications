64b19315cfc2a2fcb42de99648255f1ffd40ff7f
mobin-newroz <ahmobin@newroztech.com>
2023-05-30 19:17:23 +0600

    chore : spring search specifications

---
+++ src/main/java/mobin/learn/searchspecification/SearchSpecificationApplication.java
@@ -0,0 +1,13 @@
+package mobin.learn.searchspecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSpecificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchSpecificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mobin/learn/searchspecification/SearchSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package mobin.learn.searchspecification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSpecificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
